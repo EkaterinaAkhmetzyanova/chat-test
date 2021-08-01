e9046fc8f656367000d1e20292f7b0bb5daa6475
Ekaterina Akhmetzyanova <a.ekaterina323@gmail.com>
2021-08-01 13:24:55 +0300

    rewrite

--- server.js
+++ server.js
@@ -1,87 +1,144 @@
 const http = require('http');
 const port = process.env.PORT || 8080;
 const Koa = require('koa');
-const koaBody = require('koa-body');
+const cors = require('koa2-cors');
+//const koaBody = require('koa-body');
 const WS = require('ws');
-const uuid = require('uuid');
-const Router = require('koa-router');
+//const uuid = require('uuid');
+//const Router = require('koa-router');
 const app = new Koa();
 const server = http.createServer(app.callback());
 const wsServer = new WS.Server({server});
-const router = new Router();
+//const router = new Router();
 const users = [];
 
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    'Access-Control-Allow-Origin': true,
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
 
-app.use(koaBody({
-    urlencoded: true,
-    multipart: true,
-    text: true,
-    json: true,
-}));
+// app.use(koaBody({
+//     urlencoded: true,
+//     multipart: true,
+//     text: true,
+//     json: true,
+// }));
 
-app.use(async (ctx, next) => {
-  const origin = ctx.request.get('Origin');
-  if (!origin) {
-    return await next();
-  }
-  const headers = { 'Access-Control-Allow-Origin': '*', };
+// app.use(async (ctx, next) => {
+//   const origin = ctx.request.get('Origin');
+//   if (!origin) {
+//     return await next();
+//   }
+//   const headers = { 'Access-Control-Allow-Origin': '*', };
 
-  if (ctx.request.method !== 'OPTIONS') {
-    ctx.response.set({...headers});
-    try {
-      return await next();
-    } catch (e) {
-      e.headers = {...e.headers, ...headers};
-      throw e;
-    }
-  }
+//   if (ctx.request.method !== 'OPTIONS') {
+//     ctx.response.set({...headers});
+//     try {
+//       return await next();
+//     } catch (e) {
+//       e.headers = {...e.headers, ...headers};
+//       throw e;
+//     }
+//   }
 
-  if (ctx.request.get('Access-Control-Request-Method')) {
-    ctx.response.set({
-      ...headers,
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-    });
+//   if (ctx.request.get('Access-Control-Request-Method')) {
+//     ctx.response.set({
+//       ...headers,
+//       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
+//     });
 
-    if (ctx.request.get('Access-Control-Request-Headers')) {
-      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
-    }
-    ctx.response.status = 204;
-  }
-});
+//     if (ctx.request.get('Access-Control-Request-Headers')) {
+//       ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
+//     }
+//     ctx.response.status = 204;
+//   }
+// });
 
-router.get('/users', async (ctx, next) => {
-  ctx.response.body = users;
-});
+// router.get('/users', async (ctx, next) => {
+//   ctx.response.body = users;
+// });
 
-router.post('/users/:name', async (ctx, next) => {
-  const user = {...ctx.request.body, id: uuid.v4()};
-  users.push(user);
-  ctx.response.status = 200;
-});
+// router.post('/users/:name', async (ctx, next) => {
+//   const user = {...ctx.request.body, id: uuid.v4()};
+//   users.push(user);
+//   ctx.response.status = 200;
+// });
 
-router.delete('users/:name', async (ctx, next) => {
-  const index = users.findIndex(({name}) => name === ctx.params.name);
-  if (index !== -1) {
-    users.splice(index, 1);
-  }
-  ctx.response.status = 200;
-});
+// router.delete('users/:name', async (ctx, next) => {
+//   const index = users.findIndex(({name}) => name === ctx.params.name);
+//   if (index !== -1) {
+//     users.splice(index, 1);
+//   }
+//   ctx.response.status = 200;
+// });
 
 wsServer.on('connection', (ws, req) => {
-  const arr = [...wsServer.users];
   ws.on('message', (msg) => {
-    
-    arr.filter((el) => {
-      return el.readyState === WS.OPEN;
-    })
-    .forEach((el) => el.send(JSON.stringify({type: 'delete user'})));
-    ws.close();
-  });
-  arr.filter((el) => {
-    return el.readyState === WS.OPEN;
-  })
-  .forEach((el) => el.send(JSON.stringify({type: 'add user'})));
+    const request = JSON.parse(msg);
+    if (request.event === 'login') {
+      const nickname = users.findIndex((item) => item.name.toLowerCase() === request.message.toLowerCase());
+      if (request.message && nickname === -1) {
+        ws.name = request.message;
+        const userList = users.map((item) => item.name);
+        ws.send(JSON.stringify(
+          {
+            event: 'connect',
+            message: {
+              action: 'login',
+              name: ws.name,
+            }
+          }
+        ));
+        users.push(ws);
+        users.forEach((item) => {
+          const userMsg = JSON.stringify({
+            event: 'system',
+            message: {
+              action: 'login',
+              name: ws.name,
+            }
+          });
+          item.send(userMsg);
+        });
+      } else {
+        ws.close(1000, 'Такой псевдоним уже занят');
+      }
+    }
+    if (request.event === 'chat') {
+      users.forEach((item) => {
+        const userMsg = JSON.stringify({
+          event: 'chat',
+          message: {
+            name: ws.name,
+            created: new Date().toLocaleString('ru');
+            text: request.message,
+          }
+        });
+        item.send(userMsg);
+      })
+    }
 });
 
-app.use(router.routes()).use(router.allowedMethods());
-server.listen(port);
+ws.on('close', () => {
+  const nickname = users.findIndex((item) => item.name === ws.name);
+  if (nickname !== -1) {
+    users.splice(nickname, 1);
+    users.forEach((item) => {
+      const userMsg = JSON.stringify({
+        event: 'system',
+        message: {
+          action: 'logout',
+          name: ws.name,
+        }
+      });
+      item.send(userMsg);
+    })
+  }
+});
+});
+
+server.listen(port, () => console.log('Server started'));
